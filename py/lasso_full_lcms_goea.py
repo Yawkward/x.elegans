acc4cf36cd9d083c1296620b021d720a7d930e30
yawkward <york360@proton.me>
2024-01-30 13:56:18 +0100

    lcms goea.py

--- py/lasso_full_lcms_goea.py
+++ py/lasso_full_lcms_goea.py
@@ -287,3 +287,167 @@ def goea_results_to_file(goea_results, obo_path, path='./lasso_models/10xKfold_l
         enrichment_df.to_csv(enrichment_file_name)
         print(f"Enrichment matrix saved to {enrichment_file_name}")
 
+
+lcms_categories2 = {
+    "Amino_Acids_and_Derivatives": [
+        "Argininosuccinic_acid_L", "Aspartyphenylalanine_1_L", "gamma_Glutamylisoleucine_", 
+        "gamma_Glutamylvaline_", "Glycyl_L_phenylalanine_L", "Histidine_L", 
+        "L_gamma_glutamyl_L_isoleucine", "N_gamma_L_Glutamyl_L_methionin", "Phenylalanylglutamic_acid_", 
+        "Prolylglycine_L", "Prolyphenylalanine_or_Leucymet", "Tyrosine_L", "Kynurenine_free_base_L", "N_Decanoylglycine_",
+        "Phenylalanyaspartatic_acid_L"
+    ],
+    "Quinic_Acid_Derivatives": [
+        "_3_5_dicaffeoul_quinic_acid_L", "_1_3_Dicaffeoylquinic_acid_", "_4_O_p_Coumaroylquinic_acid_", 
+        "_4_5_Di_O_caffeoylquinic_acid_", "Coumaroyl_quinic_acid_", "Caffeoylshikimic_acid_L","Quinic_acid_161_05_", 
+        "Quinic_acid_derivative_3_56", "Quinic_acid_derivative_with_he"
+    ],
+    "Phenolic_Compounds": [
+        "Caffeic_acid_1_L", "Caffeic_acid_glucoside_L", "Caffeoylglucarate_L", 
+        "Chlorogenic_acid_", "Conduritol_B_Epoxide_L", "Dehydro_Ascorbic_acid_L", "Ferulic_acid_4_O_glucuronide_L", 
+        "Feruloyl_di_glucoside_L", "Feruloylglucose_L", "Flavonoid_glycoside_", "Galloylglucose_L", 
+        "Gentesic_acid_glucoside_L", "Kaempferol_3_O_glucoside_7_O_r", "Kaempferol_3_O_rhamnoside_7_O_", 
+        "Kaempferol_3_O_rutinoside_", "Kaempferol_7_3G_glucosylgentio", "Kaempferol_7_O_Glucoside_1_L", 
+        "Kaempferol_glucose_xylose_L", "Kaempferol_rhamnose_glucose", "Naringenin_chalcone_L", "p_coumaroyl_di_glucoside_L", 
+        "p_coumaroyl_glucoside_L", "Phenolic_glycoside_", "Quercetin_3_7_di_glucoside_", "Quercetin_3_glucoside_3", 
+        "Quercetin_7_4_O_diglucoside_", "Quercetin_glc_glc_rha", "Quercetin_glucose_glucose_rham", 
+        "Quercetin_glucose", "Quercetin_glucose_xylose", "Quercitin_3_7_diglucoside_6_rh", "Quercitin_3_O_Glucoside_2_L", 
+        "Resveratrol_3_4_diglucoside_L", "Sinapoylglucose_2_L", "Sinapoylglucose_L","Catechol_", "_5_Chlorogenic_acid", "chlorgenic_acid_L", 
+        "hydroxyjasmonic_acid_glucoside", "Hydroxy_jasmonic_acid_glucosid"
+    ],
+    "Sugars_and_Sugar_Derivatives": [
+        "alpha_D_Galacturonic_acid_1_ph", "alpha_L_Fucose_1_Phosphate", "Dihydrophaseic_acid_glucoside_", 
+        "Dihydroxyacetone_phosphate_L", "Fructose_1_6_biosphasphate_L", "Glucaric_acid_1_4_lactone_L", 
+        "Gluconic_acid_lactone_L", "Gluconic_acid_L", "Glutamic_acid_L", "Inosine_L", "Isorhamnetin_3_O_glucoside_L", 
+        "Isorhamnetin_3_O_rutinoside", "Lactobionic_acid_L", "Maltotriose_", "naringenin_7_O_glucoside_1_L", 
+        "naringenin_7_O_glucoside_3_L", "Pantothenic_acid_L", "Phaseoloidin", 
+        "Pyroglutamic_acid_3_L", "Ribulose_5_phosphate_L", "Tartaric_acid_L", "Trehalose", 
+        "Trehalose_phenolic_acid_", "Vanilloloside", "Vanilloside_L", "Zeatin_glucoside_L", "_1_O_Feruloyl_glucose_L"
+    ],
+    "Nucleotides_and_Derivatives": [
+        "_5_Deoxy_5_Methylthioadenosine", "Guanosine_", "Nicotinamide_adenine_dinucleot", 
+        "Oxidized_glutathione_",
+    ],
+    "Terpenes_and_Triterpenes": [
+        "Diterpene_8_2", "Terpene_10_26", "Terpene_9_53", "Triterpene_10_43", "Triterpene_10_78", 
+        "Triterpene_8_14", "Triterpene_8_21", "Triterpene_9_79"
+    ],
+    "Saponins": [
+        "Saponin_10_182", "Saponin_10_183", "Saponin_10_264", "Saponin_10_47", "Saponin_10_56", 
+        "Saponin_10_67780171", "Saponin_10_72", "Saponin_8_02", "Saponin_8_42", "Saponin_8_83", 
+        "Saponin_8_8", "Saponin_9_18", "Saponin_9_88", "Soyasaponin_A2_L"
+    ],
+    "Porphyrins": [
+        "porphobilinogen_2_L"
+    ],
+    "Flavonoids": [
+        "_Rutin"
+    ],
+    "Other": [
+        "_10_Formyltetrahydrofolate_L", "_3_Deoxy_D_manno_2_octulosonic", "_5_hydroxy_Ferulic_acid_Glucos2", 
+        "_5_hydroxy_ferulic_acid_glucos", "_6_phosphogluconic_acid_L", "_7_Epi_12_hydroxyjasmonic_acid", 
+        "_D_Glycero_alpha_D_Manno_Hepto", "_R_2_Phenylglycin", "Azelaic_acid_L", "Azukisaponin_VI_1_L", 
+        "CGA_hexose_", "Urocanic_acid_L"
+    ],
+    "unidentified": [
+        "_1021_486521_9_141280832",
+        "_1021_48667462_9_66538870707",
+        "_1063_497231_10_58247297",
+        "_1063_529871_9_415000766",
+        "_1141_528589_8_043846139",
+        "_1151_551493_10_40677464",
+        "_1165_527084_8_995631942",
+        "_1183_537778_8_600427106",
+        "_1183_538043_8_673358308",
+        "_1191_542641_10_3213884",
+        "_1209_556345_10_14653119",
+        "_1241_580464_8_916526509",
+        "_1253_57885907_8_99579782362",
+        "_1283_593009_9_259004534",
+        "_1283_593348_9_160768845",
+        "_1313_601235_9_01121774",
+        "_303_0720727_3_335904942",
+        "_312_0943959_2_614228811",
+        "_351_1294837_5_809380415",
+        "_366_9939681_1_170199352",
+        "_374_1568694_3_674168475",
+        "_380_1288743_6_36896135",
+        "_427_1824019_5_95992104",
+        "_429_1765684_7_873841266",
+        "_441_1978503_6_218123415",
+        "_475_1821542_6_560220101",
+        "_487_2029578_5_338775921",
+        "_496_1502126_3_792994891",
+        "_512_1445532_3_132648409",
+        "_523_1663837_5_238225973",
+        "_531_2448764_7_209307815",
+        "_567_2828405_6_133036019",
+        "_641_171822_6_885700966",
+        "_658_1575839_4_528042834",
+        "_671_2775803_7_254060113",
+        "_821_3258533_9_228249892",
+        "_857_418157679_9_32212355859",
+        "_857_418693067_9_24167206032",
+        "_931_4518066_8_63280459",
+        "_933_4699157_8_265868462",
+        "_947_486080952_9_72692719903",
+        "_963_4798371_8_624190173",
+        "_987_4809544_10_24318525"
+    ]
+}
+
+
+
+# In[46]:
+
+def goea_for_category(category, metabolites, path):
+    #excel_writer = pd.ExcelWriter(f'{outpath}{category}.xlsx', engine='xlsxwriter')
+    goea_results = {}
+    print(category)
+    for m in metabolites:
+        print(os.path.join(path, f"{m}_nXcv.json"))
+        file_path = os.path.join(path, f"{m}_nXcv.json")
+        with open(file_path, 'r') as json_file:
+            data = json.load(json_file)
+        
+        # if no feature have been selected
+        if len(data['selected_features']) == 0:
+            print(f"Warning: {file_path} has no selected_features\n")
+            continue
+
+        # perform GOEA
+        data_matched, data_goids, data_term_count, data_goea_all = matchgff2(data['selected_features'], namespace=['molecular_function'], depth_threshold=2, goea=True)
+        goea_results[m] = data_goea_all
+    
+    return goea_results
+
+
+# In[50]:
+
+path_gc = "/work/yhesse/PW_rawdata/results/full_lasso/lcms"
+
+
+for category, metabolites in lcms_categories2.items():
+    # check if output directories exist otherwise createt them
+    category_path = os.path.join(path_gc, 'goea', f"{category}/")
+    if not os.path.exists(category_path):
+        try:
+            # If the path does not exist, create the directory
+            os.makedirs(category_path)
+            print(f"Directory created: {category_path}")
+        except Exception as e:
+            # If there is an error in creating the directory, raise an error
+            raise Exception(f"Error creating directory: {category_path}. Error: {e}")
+    else:
+        print(f"Path already exists: {category_path}")
+
+    print(category_path)
+
+    # do GOEA for each metabolite in the category and save them into 
+    # dictionary where each key is the prefix before _nXcv.json
+    lasso_goea = goea_for_category(category, metabolites, path=path_gc)
+
+    # save the goea results to .csv and excel tables and create enrichment matrix .csv
+    goea_results_to_file(lasso_goea, path=category_path, obo_path="/work/yhesse/PW_rawdata/go_obo/go.obo", to_excel=True)
+
+    
+
